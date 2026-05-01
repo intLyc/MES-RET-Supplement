2a8946ceb16718b1877445a783c17a94b20dc7c5
Yanchi Li <int_lyc@cug.edu.cn>
2026-05-01 10:53:06 +0800

    Update MaT_Gym.m

--- Source-Code/Problems/MaT-Gym/MaT_Gym.m
+++ Source-Code/Problems/MaT-Gym/MaT_Gym.m
@@ -8,6 +8,24 @@ classdef MaT_Gym < Problem
 %   - Dynamic Neural Network Architecture.
 %   - Online Input Normalization (Welford's Algorithm).
 
+%------------------------------- Reference --------------------------------
+% @InProceedings{Li2026MES-RET,
+%   author    = {Li, Yanchi and Liu, Jiao and Gong, Wenyin and Gu, Qiong and Zhao, Yue and Ong, Yew-Soon},
+%   booktitle = {Forty-third International Conference on Machine Learning},
+%   title     = {Breaking Multi-Task Curse: Reward-Weighted Evolution for Black-Box Many-Task Optimization},
+%   year      = {2026},
+%   url       = {https://openreview.net/forum?id=lkGnJhXUNu},
+% }
+%--------------------------------------------------------------------------
+
+%------------------------------- Copyright --------------------------------
+% Copyright (c) Yanchi Li. You are free to use the MToP for research
+% purposes. All publications which use this platform should acknowledge
+% the use of MToP and cite as "Y. Li, W. Gong, T. Zhang, F. Ming,
+% S. Li, Q. Gu, and Y.-S. Ong, MToP: A MATLAB Benchmarking Platform for
+% Evolutionary Multitasking, 2023, arXiv:2312.08134"
+%--------------------------------------------------------------------------
+
 properties (Access = public)
     % --- Hyperparameters ---
     hiddenLayers = 2; % Network depth
